c483781c9e738e2659e7227fdcbf8c83d6e213dc
nikkbarz <muniquebarboza1@gmail.com>
2025-07-29 19:59:47 -0300

    main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000, '0.0.0.0'); // <- ESSA LINHA É A CHAVE
+
+  // Permitindo acesso de qualquer origem (CORS)
+  app.enableCors();
+
+  // Railway define a porta via variável de ambiente PORT
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`🚀 Server is running on http://localhost:${port}`);
 }
 bootstrap();
-
