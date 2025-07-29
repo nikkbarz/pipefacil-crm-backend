5139a84fa7c0fa915d03f82fbaaebc5ad34b5576
nikkbarz <muniquebarboza1@gmail.com>
2025-07-29 20:39:33 -0300

    lead.controller.ts

--- src/lead.controller.ts
+++ src/lead.controller.ts
@@ -1,1 +1,14 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { LeadService } from './lead.service';
 
+@Controller('leads')
+export class LeadController {
+  constructor(private readonly leadService: LeadService) {}
+
+  @Post()
+  async createLead(
+    @Body() body: { nome: string; email: string; telefone: string; origem: string },
+  ) {
+    return this.leadService.createLead(body);
+  }
+}
